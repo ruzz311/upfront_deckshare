34d47736979f40df747db85962dd49ada3685ca6
Russell Madsen <ruzz311@gmail.com>
2011-11-30 05:48:59 -0600

    socket.io

--- server.js
+++ server.js
@@ -44,15 +44,15 @@ app.configure(function(){
   app.use( express.methodOverride() );
   app.use( express.cookieParser() );
   app.use( express.session({ secret: 'your secret here' }) );
-  app.use( stylus.middleware({ 
-      src: appdir['public'],
-      compile: compile 
-    })); 
   app.use( app.router );
   app.use( express['static'](appdir['public']) );
 });
 
 app.configure( 'development', function( ) {
+  app.use( stylus.middleware({ 
+      src: appdir['public'],
+      compile: compile 
+    })); 
   app.use( express.errorHandler({ dumpExceptions: true, showStack: true }) ); 
 });
 
